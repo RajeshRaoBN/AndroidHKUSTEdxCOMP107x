3ab8f4b7cff2793c3284b71df487b67d93bb6dbf
Rajesh <mailrbn@gmail.com>
2018-05-07 10:12:13 +1000

    Inter7/5/18

--- GreetFriend/app/src/main/java/hk/ust/cse/comp107x/greetfriend/MainActivity.java
+++ GreetFriend/app/src/main/java/hk/ust/cse/comp107x/greetfriend/MainActivity.java
@@ -2,9 +2,13 @@ package hk.ust.cse.comp107x.greetfriend;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import java.util.Calendar;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +24,26 @@ public class MainActivity extends AppCompatActivity {
         greetButton = (Button) findViewById(R.id.greetbutton);
         greetings = (TextView) findViewById(R.id.friendGreeting);
         friendsName = (EditText) findViewById(R.id.editFriendName);
+    }
+
+    public void changeGreetings(View view) {
+
+        Date date = new Date();
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        int hour = cal.get(Calendar.HOUR_OF_DAY);
+
+        String greetingSuffix;
+
+        if(hour > 6 && hour < 12)
+            greetingSuffix = "Morning";
+        else if(hour > 12 && hour < 17)
+            greetingSuffix = "Afternoon";
+        else if(hour > 17 && hour < 21)
+            greetingSuffix = "Evening";
+        else
+            greetingSuffix = "Night";
+
+        greetings.setText("Good " + greetingSuffix + " " +friendsName.getText().toString() + "!");
     }
 }
